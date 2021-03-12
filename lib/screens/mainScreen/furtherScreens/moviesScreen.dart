32acdc513c14320b5128bbf27696bc91444f4951
imark00 <marksonline247@gmail.com>
2021-03-12 13:29:23 +0000

    updates

--- lib/screens/mainScreen/furtherScreens/moviesScreen.dart
+++ lib/screens/mainScreen/furtherScreens/moviesScreen.dart
@@ -13,6 +13,7 @@ class _MoviesScreenState extends State<MoviesScreen> {
   @override
   void initState() {
     Movies.getUpcomingMovies();
+    Movies.getTopRatedMovies();
     super.initState();
   }
 
@@ -25,6 +26,7 @@ class _MoviesScreenState extends State<MoviesScreen> {
           SizedBox(
             height: 8.0,
           ),
+          _topRatedMovies(),
         ],
       ),
     );
@@ -42,7 +44,7 @@ class _MoviesScreenState extends State<MoviesScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  'Upcoming Movies',
+                  'Upcoming',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20.0,
@@ -84,8 +86,96 @@ class _MoviesScreenState extends State<MoviesScreen> {
                               image: Movies.upcomingMovies.length == null
                                   ? AssetImage(
                                       'assets/icons/logoTransparent.png')
-                                  : NetworkImage(portraitImagePath(Movies
-                                      .upcomingMovies[index]['poster_path'])),
+                                  : NetworkImage(
+                                      portraitImagePath(
+                                          Movies.upcomingMovies[index]
+                                              ['poster_path']),
+                                    ),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }
+                return Shimmer.fromColors(
+                  baseColor: Colors.red,
+                  highlightColor: Colors.yellow,
+                  child: Text(
+                    'Shimmer',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                );
+              })
+        ],
+      ),
+    );
+  }
+
+  Widget _topRatedMovies() {
+    return Container(
+      height: 250.0,
+      width: double.infinity,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Top Rated',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    print('view more');
+                  },
+                  padding: EdgeInsets.all(0.0),
+                  splashColor: Colors.white12,
+                  child: Text(
+                    'view more',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          FutureBuilder(
+              future: Movies.getTopRatedMovies(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Expanded(
+                    child: Container(
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 10,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) => Container(
+                          width: 120.0,
+                          margin: EdgeInsets.only(left: 10.0),
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: Movies.topRatedMovies.length == null
+                                  ? AssetImage(
+                                      'assets/icons/logoTransparent.png')
+                                  : NetworkImage(
+                                      portraitImagePath(
+                                          Movies.topRatedMovies[index]
+                                              ['poster_path']),
+                                    ),
                               fit: BoxFit.fill,
                             ),
                           ),
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -6,7 +6,6 @@ const kURL = 'api.themoviedb.org/3';
 
 class TrendingMovies {
   static List trendingMovies;
-
   static Future getTrendingMovies() async {
     try {
       http.Response response =
@@ -28,7 +27,6 @@ class TrendingMovies {
 
 class TrendingTVShows {
   static List trendingTVShows;
-
   static Future getTrendingTVShows() async {
     try {
       http.Response response =
@@ -68,11 +66,20 @@ class Movies {
     }
   }
 
-  static List nowPlayingMovies;
-  static Future<List> getNowPlayingMovies() async {
+  static List topRatedMovies;
+  static Future<List> getTopRatedMovies() async {
     try {
       http.Response response = await http.get(
-          'https://$kURL/movie/now_playing?api_key=$kApiKey&language=en-US&page=1');
+          'https://$kURL/movie/top_rated?api_key=$kApiKey&language=en-US&page=1');
+
+      if (response.statusCode == 200) {
+        Map mapResponse = jsonDecode((response.body));
+        topRatedMovies = mapResponse['results'];
+        return topRatedMovies;
+      } else {
+        print(response.statusCode);
+        throw ('Failed');
+      }
     } catch (e) {
       print(e);
       return (e);
